e4dc84a44ee13a972897fcb987f64bd7bba8167b
wuusaaa <razzelinger@gmail.com>
2016-12-22 21:48:29 +0200

    changes

--- app/src/main/java/com/project/homey/ActivityLogin.java
+++ app/src/main/java/com/project/homey/ActivityLogin.java
@@ -1,8 +1,8 @@
 package com.project.homey;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,7 +21,10 @@ public class ActivityLogin extends AppCompatActivity {
                 onRegButtonClick(v);
             }
         });
+
+
     }
+
 
     private void onRegButtonClick(View v) {
         Intent intent = new Intent(this, RegistrationActivity.class);
