35e9d5a7a78b296a4b92295fac41f569a491fc51
wuusaaa <razzelinger@gmail.com>
2016-12-22 11:03:36 +0200

    changes

--- app/src/main/java/com/project/homey/ActivityLogin.java
+++ app/src/main/java/com/project/homey/ActivityLogin.java
@@ -1,13 +1,30 @@
 package com.project.homey;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityLogin extends AppCompatActivity {
+
+    private Button getMeToReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getMeToReg = (Button) findViewById(R.id.buttonRegistration);
+        getMeToReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRegButtonClick(v);
+            }
+        });
+    }
+
+    private void onRegButtonClick(View v) {
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
     }
 }
