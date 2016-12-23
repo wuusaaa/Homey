254fadc8a66f79a7675beec74a96dd41cbb6b3dd
Barakm <barakm@wix.com>
2016-12-24 01:40:06 +0200

    Changed some configuration in 'gradle' file according to add functionality with tests that I run on activities that I'll build. Added a new activity for user profile. There is a lot of work to do. Added user name and email string to string.xml. Put three_button_menu.xml in comment because it breaks the project.

--- app/src/main/java/com/project/homey/LoginActivity.java
+++ app/src/main/java/com/project/homey/LoginActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class ActivityLogin extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     private Button getMeToReg;
 
