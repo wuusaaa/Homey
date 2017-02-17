93e876f87fa36b9c69b5898c30a69ec3cbc90053
Barakm <barakm@wix.com>
2017-02-17 16:40:47 +0200

    New activity for edit profile details.

---
+++ app/src/main/java/com/project/homey/EditProfileActivity.java
@@ -0,0 +1,12 @@
+package com.project.homey;
+
+import android.os.Bundle;
+
+public class EditProfileActivity extends ProfileActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
--- app/src/main/java/com/project/homey/ProfileActivity.java
+++ app/src/main/java/com/project/homey/ProfileActivity.java
@@ -1,7 +1,9 @@
 package com.project.homey;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void editButton_onClick(View view) {
+        Intent intent = new Intent(this, EditProfileActivity.class);
+        startActivity(intent);
     }
 }
