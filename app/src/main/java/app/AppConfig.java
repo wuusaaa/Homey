8b7efd11a19d17750280088b8f6cedce7dcb125d
razze <razzelinger@gmail.com>
2017-02-18 15:29:47 +0200

    changes

---
+++ app/src/main/java/app/AppConfig.java
@@ -0,0 +1,13 @@
+package app;
+
+/**
+ * Created by razze on 18/02/2017.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "https://www.crossword-clues.com/homey/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "https://www.crossword-clues.com/homey/register.php";
+}
