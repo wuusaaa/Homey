d88b0502c48e0c72dcaa3844601bc7cfb770468a
Barakm <barakm@wix.com>
2017-02-19 11:32:38 +0200

    Added unit test for Task Class and some properties into it.

--- app/src/main/java/lib/Task.java
+++ app/src/main/java/lib/Task.java
@@ -5,4 +5,30 @@ package lib;
  */
 
 public class Task {
+
+    private TaskDate m_TaskDate;
+    private String m_TaskName;
+    private Boolean m_IsDone;
+    private String m_Asignee;
+
+    public Task(TaskDate i_TaskDate, String i_TaskName, String i_Asignee) {
+        m_TaskDate = i_TaskDate;
+        m_TaskName = i_TaskName;
+        m_IsDone = false;
+        m_Asignee = i_Asignee;
+    }
+
+    public void setTaskAsComplete() {
+        m_IsDone = true;
+    }
+
+    public void setTaskAsInComplete() {
+        m_IsDone = false;
+    }
+
+    public Boolean isComplete() {
+        return m_IsDone;
+    }
+
+
 }
---
+++ app/src/main/java/lib/TaskDate.java
@@ -0,0 +1,17 @@
+package lib;
+
+import android.location.Location;
+
+import java.sql.Time;
+import java.util.Date;
+
+/**
+ * Created by barakm on 19/02/2017.
+ */
+public class TaskDate {
+    Date m_DueDate;
+    Location m_Location;
+    Time m_DueTime;
+    int m_Duration;
+
+}
