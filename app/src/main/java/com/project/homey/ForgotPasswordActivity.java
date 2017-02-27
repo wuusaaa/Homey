b018054923c8f9467b0401430e7354b85e61f91d
razze <razzelinger@gmail.com>
2017-02-27 13:49:02 +0200

    added forgot password activity and button in login activity

---
+++ app/src/main/java/com/project/homey/ForgotPasswordActivity.java
@@ -0,0 +1,13 @@
+package com.project.homey;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+    //TODO
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
