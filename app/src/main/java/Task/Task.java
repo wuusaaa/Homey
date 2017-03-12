398b79d211bb6ebb0364adcb1ee718f001c20447
razze <razzelinger@gmail.com>
2017-03-12 18:09:54 +0200

    added "addTask" to database api with all the dependencies needed

--- app/src/main/java/Task/Task.java
+++ app/src/main/java/Task/Task.java
@@ -1,34 +1,101 @@
 package task;
 
+import java.util.Date;
+
 /**
  * Created by Raz on 12/20/2016.
  */
 
 public class Task {
 
-    private TaskDate m_TaskDate;
-    private String m_TaskName;
-    private Boolean m_IsDone;
-    private String m_Asignee;
+    private String name;
+    private String description;
+    private String status;
+    private String location;
+    private int creatorId;
+    private Date startTime;
+    private Date endTime;
 
-    public Task(TaskDate i_TaskDate, String i_TaskName, String i_Asignee) {
-        m_TaskDate = i_TaskDate;
-        m_TaskName = i_TaskName;
-        m_IsDone = false;
-        m_Asignee = i_Asignee;
+    public String GetName() {
+        return name;
     }
 
-    public void setTaskAsComplete() {
-        m_IsDone = true;
+    public void SetName(String name) {
+        this.name = name;
     }
 
-    public void setTaskAsInComplete() {
-        m_IsDone = false;
+    public String GetDescription() {
+        return description;
     }
 
-    public Boolean isComplete() {
-        return m_IsDone;
+    public void SetDescription(String description) {
+        this.description = description;
     }
+
+    public String GetStatus() {
+        return status;
+    }
+
+    public void SetStatus(String status) {
+        this.status = status;
+    }
+
+    public String GetLocation() {
+        return location;
+    }
+
+    public void SetLocation(String location) {
+        this.location = location;
+    }
+
+    public int GetCreatorId() {
+        return creatorId;
+    }
+
+    public void SetCreatorId(int creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public Date GetStartTime() {
+        return startTime;
+    }
+
+    public void SetStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date GetEndTime() {
+        return endTime;
+    }
+
+    public void SetEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+
+//    private TaskDate m_TaskDate;
+//    private String m_TaskName;
+//    private Boolean m_IsDone;
+//    private String m_Asignee;
+//
+//    public Task(TaskDate i_TaskDate, String i_TaskName, String i_Asignee) {
+//        m_TaskDate = i_TaskDate;
+//        m_TaskName = i_TaskName;
+//        m_IsDone = false;
+//        m_Asignee = i_Asignee;
+//    }
+//
+//    public void setTaskAsComplete() {
+//        m_IsDone = true;
+//    }
+//
+//    public void setTaskAsInComplete() {
+//        m_IsDone = false;
+//    }
+//
+//    public Boolean isComplete() {
+//        return m_IsDone;
+//    }
 
 
 }
