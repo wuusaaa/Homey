c9bd712d6768f52c130331e7d6d014952e023bc8
wuusaaa <razzelinger@gmail.com>
2017-07-06 11:42:43 +0300

    Added splash screen

---
+++ app/src/main/java/app/activities/SplashActivity.java
@@ -0,0 +1,21 @@
+package app.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pdc5np on 7/6/2017.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Start home activity
+        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+        // close splash activity
+        finish();
+    }
+}
