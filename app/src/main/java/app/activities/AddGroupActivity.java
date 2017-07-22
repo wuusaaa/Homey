93ba8aff9d6e57ebf06716a186cde40a30dd7a9f
talza <talzamir92@gmail.com>
2017-07-22 13:07:04 +0300

    Creare group page changes: fields, picture, users and bottom page.

--- app/src/main/java/app/activities/AddGroupActivity.java
+++ app/src/main/java/app/activities/AddGroupActivity.java
@@ -22,17 +22,17 @@ public class AddGroupActivity extends ActivityBase {
 
     public void buttonAddGroup_onClick(View view) {
         GroupManager groupManager = (GroupManager) Services.GetService(GroupManager.class);
-        String name = ((EditText) findViewById(R.id.editTextName)).getText().toString();
+        String name = ((EditText) findViewById(R.id.editTextGroupName)).getText().toString();
         byte[] arr = {0, 5, 3, 2, 4, 5};
         groupManager.AddNewGroup(name, arr, new GroupCallBack() {
             @Override
             public void onSuccess(Group group) {
-                ((EditText) findViewById(R.id.editTextName)).setText("blabla");
+                ((EditText) findViewById(R.id.editTextGroupName)).setText("blabla");
             }
 
             @Override
             public void onFailure(String error) {
-                ((EditText) findViewById(R.id.editTextName)).setText(error);
+                ((EditText) findViewById(R.id.editTextGroupName)).setText(error);
             }
         });
     }
