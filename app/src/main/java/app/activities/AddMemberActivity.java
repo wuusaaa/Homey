e235b924ccd0a7ea3a5d4fd33963b1103d9ad1f4
Barakm <barakm@wix.com>
2017-07-25 18:36:30 +0300

    Added add member activity

---
+++ app/src/main/java/app/activities/AddMemberActivity.java
@@ -0,0 +1,16 @@
+package app.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.project.homey.R;
+
+
+public class AddMemberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_member);
+    }
+}
