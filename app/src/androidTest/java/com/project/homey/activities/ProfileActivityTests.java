6f31c492c8497a8f0598dc198d1645fbdca9054c
Barakm <barakm@wix.com>
2017-08-01 17:20:51 +0300

    Added two spinners to group page activity.

--- app/src/androidTest/java/com/project/homey/activities/ProfileActivityTests.java
+++ app/src/androidTest/java/com/project/homey/activities/ProfileActivityTests.java
@@ -3,19 +3,11 @@ package com.project.homey.activities;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.project.homey.R;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import app.activities.ProfileActivity;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by barakm on 23/12/2016
@@ -30,31 +22,5 @@ public class ProfileActivityTests {
 
     @Test
     public void hasUserNameTextView() {
-        onView(withId(R.id.textViewUserNameLabel)).check(matches(isDisplayed()));
-        onView(withId(R.id.textViewUserNameLabel)).check(matches(withText("User Name:")));
-
-    }
-
-    @Test
-    public void hasUsersUserName() {
-        onView(withId(R.id.textViewUserNameValue)).check(matches(isDisplayed()));
-        onView(withId(R.id.textViewUserNameValue)).check(matches(withText("")));
-    }
-
-    @Test
-    public void hasEmailTextView() {
-        onView(withId(R.id.textViewEmailLabel)).check(matches(isDisplayed()));
-        onView(withId(R.id.textViewEmailLabel)).check(matches(withText("Email:")));
-    }
-
-    @Test
-    public void hasUsersEmailTextView() {
-        onView(withId(R.id.textViewEmailValue)).check(matches(isDisplayed()));
-        onView(withId(R.id.textViewEmailValue)).check(matches(withText("")));
-    }
-
-    @Test
-    public void hasUsersProfilePicture() {
-        onView(withId(R.id.imageButtonUsersProfilePicture)).check(matches(isDisplayed()));
     }
 }
