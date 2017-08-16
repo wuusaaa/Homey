2b52a5dc86bf7b1401a17b3349ab555cab52c6e4
Barakm <barakm@wix.com>
2017-08-16 22:32:57 +0300

    Added remove member from group

--- app/src/main/java/app/customcomponents/ScrollHorizontalWithItems.java
+++ app/src/main/java/app/customcomponents/ScrollHorizontalWithItems.java
@@ -2,28 +2,21 @@ package app.customcomponents;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.drawable.GradientDrawable;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.PopupMenu;
 import android.util.AttributeSet;
-import android.util.Pair;
 import android.view.DragEvent;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.HorizontalScrollView;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.project.homey.R;
 
 import java.util.ArrayList;
-import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -31,7 +24,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 import app.activities.interfaces.IHasImage;
 import app.activities.interfaces.IHasText;
 import app.logic.appcomponents.Group;
-import app.logic.appcomponents.Task;
 import app.logic.appcomponents.User;
 import app.logic.managers.DBManager;
 import app.logic.managers.DragManager;
@@ -39,8 +31,7 @@ import app.logic.managers.Services;
 import callback.GotoGroupPageCallBack;
 import callback.UpdateCallBack;
 
-public class ScrollHorizontalWithItems extends HorizontalScrollView
-{
+public class ScrollHorizontalWithItems extends HorizontalScrollView {
     private final LinearLayout linearLayout;
     private HashMap itemsMap = new HashMap();
 
@@ -58,8 +49,7 @@ public class ScrollHorizontalWithItems extends HorizontalScrollView
         this.addView(linearLayout);
     }
 
-    public ScrollHorizontalWithItems(Context context, AttributeSet attrs, int defStyle)
-    {
+    public ScrollHorizontalWithItems(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
 
         linearLayout = new LinearLayout(context);
@@ -78,8 +68,7 @@ public class ScrollHorizontalWithItems extends HorizontalScrollView
         setLayoutParamsMargin(layoutParamsImageButton);
         setLayoutParamsMargin(layoutParamsTextView);
 
-        for (T item : items)
-        {
+        for (T item : items) {
             LinearLayout verticalLinearLayout = new LinearLayout(this.getContext());
             CircleImageButton itemIcon = new CircleImageButton(this.getContext(), item.GetImage(), R.mipmap.ic_group_default);
             TextView textView = new TextView(this.getContext());
@@ -107,28 +96,22 @@ public class ScrollHorizontalWithItems extends HorizontalScrollView
         }
     }
 
-    private <T extends IHasText & IHasImage> void setGroupIconDrop(CircleImageButton itemIcon, T item)
-    {
+    private <T extends IHasText & IHasImage> void setGroupIconDrop(CircleImageButton itemIcon, T item) {
         DragManager dragManager = (DragManager) Services.GetService(DragManager.class);
 
         itemIcon.setOnDragListener(new OnDragListener() {
             @Override
-            public boolean onDrag(View view, DragEvent dragEvent)
-            {
+            public boolean onDrag(View view, DragEvent dragEvent) {
                 int action = dragEvent.getAction();
-                switch (action)
-                {
+                switch (action) {
                     case DragEvent.ACTION_DRAG_STARTED:
                         //none
                         break;
                     case DragEvent.ACTION_DRAG_ENTERED:
-                        if (item instanceof User)
-                        {
+                        if (item instanceof User) {
                             dragManager.setDroppedUser((User) item);
                             view.setBackgroundResource(R.color.light_gray);
-                        }
-                        else
-                        {
+                        } else {
                             dragManager.setDroppedUser(null);
                         }
 
@@ -152,58 +135,60 @@ public class ScrollHorizontalWithItems extends HorizontalScrollView
         });
     }
 
-    public int getDpSize(int size)
-    {
+    public int getDpSize(int size) {
         float scale = getResources().getDisplayMetrics().density;
         return (int) (size * scale + 0.5f);
     }
 
-    private void setLayoutParamsMargin(LayoutParams layoutParams)
-    {
+    private void setLayoutParamsMargin(LayoutParams layoutParams) {
         layoutParams.leftMargin = 15;
         layoutParams.rightMargin = 15;
         layoutParams.topMargin = 15;
         layoutParams.bottomMargin = 15;
     }
 
-    public void SetOnLongClick(Group group)
-    {
-        for (Object object : itemsMap.entrySet())
-        {
+    public void SetOnLongClick(Group group) {
+        for (Object object : itemsMap.entrySet()) {
             Map.Entry<String, CircleImageButton> entry = (Map.Entry<String, CircleImageButton>) object;
             CircleImageButton image = entry.getValue();
 
-            image.setOnLongClickListener( view ->
+            image.setOnLongClickListener(view ->
             {
                 PopupMenu popupMenu = new PopupMenu(getContext(), image);
                 popupMenu.getMenuInflater().inflate(R.menu.group_page_user_menu, popupMenu.getMenu());
 
                 popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
-                    public boolean onMenuItemClick(MenuItem item)
-                    {
-                        switch (item.getItemId())
-                        {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
                             case R.id.promoteAdmin:
                                 // TODO: DBMANAGER. promote user (entry.getKey(), group.getId).
                                 ((DBManager) Services.GetService(DBManager.class)).MakeUserAdmin(group.GetId(), entry.getKey(), new UpdateCallBack() {
                                     @Override
-                                    public void onSuccess()
-                                    {
+                                    public void onSuccess() {
                                         Toast.makeText(getContext(), "Promotion success", Toast.LENGTH_SHORT).show();
                                     }
 
                                     @Override
-                                    public void onFailure(String errorMessage)
-                                    {
+                                    public void onFailure(String errorMessage) {
                                         Toast.makeText(getContext(), "Promotion failed", Toast.LENGTH_SHORT).show();
                                     }
                                 });
 
                                 break;
                             case R.id.removeFromGroup:
-                                Toast.makeText(getContext(), "Remove user id: " + entry.getKey() + "from group id: "+ group.GetId(), Toast.LENGTH_SHORT).show();
-                                // TODO: DBMANAGER. remove user (entry.getKey(), group.getId).
+                                ((DBManager) Services.GetService(DBManager.class)).LeaveGroup(group.GetId(), entry.getKey(), new UpdateCallBack() {
+                                    @Override
+                                    public void onSuccess() {
+                                        Toast.makeText(getContext(), "Member has removed", Toast.LENGTH_SHORT).show();
+
+                                    }
+
+                                    @Override
+                                    public void onFailure(String errorMessage) {
+
+                                    }
+                                });
                                 break;
                             default:
                                 Toast.makeText(getContext(), "default", Toast.LENGTH_SHORT).show();
