8f05646ed951a8dbe7714e0a435a502a777826b9
Barakm <barakm@wix.com>
2017-09-05 16:38:36 +0300

    Remove location

--- app/src/main/java/app/activities/LoginActivity.java
+++ app/src/main/java/app/activities/LoginActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -45,26 +44,9 @@ public class LoginActivity extends ActivityBase {
                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                     return;
                 }
-//                mFusedLocationClient.getLastLocation()
-//                        .addOnSuccessListener(this, location -> {
-                // Got last known location. In some rare situations this can be null.
-//                                if (location != null) {
-//                                    Toast.makeText(getApplicationContext(),
-//                                            location.toString(), Toast.LENGTH_LONG)
-//                                            .show();
-//                                }
-//                        });
-
-            } else {
-
-                // permission denied, boo! Disable the
-                // functionality that depends on this permission.
             }
             return;
         }
-
-        // other 'switch' lines to check for other
-        // permissions this app might request
     }
 
     @Override
@@ -72,36 +54,7 @@ public class LoginActivity extends ActivityBase {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
-// SQLite app.database handler
         db = new SQLiteHandler(getApplicationContext());
-
-// Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_CONTACTS)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                        99);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        }
-
 
         inputEmailEditText = (EditText) findViewById(R.id.email);
         inputPasswordEditText = (EditText) findViewById(R.id.password);
@@ -118,8 +71,7 @@ public class LoginActivity extends ActivityBase {
 
 
         // Check if user is already logged in or not
-        if (((SessionManager) (Services.GetService(SessionManager.class))).isLoggedIn())
-        {
+        if (((SessionManager) (Services.GetService(SessionManager.class))).isLoggedIn()) {
             ((EnvironmentManager) (Services.GetService(EnvironmentManager.class))).SetScreenName("Home Page");
             // User is already logged in. Take him to HomePage activity
             Intent intent = new Intent(LoginActivity.this, HomePageActivity.class);
@@ -183,24 +135,23 @@ public class LoginActivity extends ActivityBase {
         pDialog.showDialog();
 
         //TODO  DBManager
-        ((DBManager) (Services.GetService(DBManager.class))).Login(email, password, new UserCallBack()
-        {
+        ((DBManager) (Services.GetService(DBManager.class))).Login(email, password, new UserCallBack() {
             @Override
-            public void onSuccess(User user)
-            {
+            public void onSuccess(User user) {
                 // Sending server DeviceId (token):
                 String token = FirebaseInstanceId.getInstance().getToken();
-                if (token == null)
-                {
+                if (token == null) {
                     token = "0";
                 }
 
-                ((DBManager)Services.GetService(DBManager.class)).UpdateUser(user.GetUserId(), "token", token, new UpdateCallBack() {
+                ((DBManager) Services.GetService(DBManager.class)).UpdateUser(user.GetUserId(), "token", token, new UpdateCallBack() {
                     @Override
-                    public void onSuccess() {}
+                    public void onSuccess() {
+                    }
 
                     @Override
-                    public void onFailure(String errorMessage) {}
+                    public void onFailure(String errorMessage) {
+                    }
                 });
 
                 // user successfully logged in
